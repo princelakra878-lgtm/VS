3b26f6f3b43470b9459b238e7ae344f73ed56d81
santkumarlakra <princelakra878@gmail.com>
2026-05-09 20:55:37 +0530

    add

---
+++ hai.js
@@ -0,0 +1,7 @@
+let a=20
+function add(){
+    var a=12
+
+}
+console.log(a)
+add();
--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function cl(a,b){
-    console.log(a + b);
-     console.log(a - b);
-      console.log(a/b);
-       console.log(a * b);
+let a=20
+function add(){
+    var b=12
+    
 }
-cl(20,2);
-cl(50,2);
-cl(60,2);
+console.log(b)
+add()
+
 
