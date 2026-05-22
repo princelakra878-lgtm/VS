5200c407a3fcfa9dde398c86a03b5be9d1ec9153
santkumarlakra <princelakra878@gmail.com>
2026-05-22 20:49:07 +0530

    array

--- pro.js
+++ pro.js
@@ -1,4 +1,19 @@
 
-let a=["arya", "prince", "rakik", "rudra"]
-console.log(a[0]);
+let a=["arya", "prince", "rakik", "rudra","priyanshu", "om"]
+console.log(a[3]);
 
+a.pop()
+console.log(a)
+
+a.shift()
+console.log(a)
+
+a.unshift("ujjwal")
+console.log(a)
+
+a.push("germany")
+console.log(a)
+
+let s=a.join("---")
+console.log(s)
+console.log(typeof(a))
